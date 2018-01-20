7b2839b48a25a11a8b149d97683420d33e5bd796
Gonzo Fialho <carloseaf@gmail.com>
2018-01-20 15:12:12 -0200

    Add level 4

---
+++ level_4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import requests
+import re
+
+p = '12345'
+url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php'
+patt = r'and the next nothing is (\d+)'
+
+while True:
+    req = requests.get(url, params={'nothing': p})
+    text = req.text
+    print(text)
+    match = re.search(patt, text)
+    if not match:
+        if 'Yes. Divide by two and keep going.' in text:
+            p = int(p) // 2
+            continue
+        break
+    p = match.group(1)
