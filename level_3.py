4ec659e776f6e2e2e0eb5d162e90d15f40c6c9b3
Gonzo Fialho <carloseaf@gmail.com>
2018-01-20 14:50:52 -0200

    Add solution to level 3

---
+++ level_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import re
+
+with open('equality.html') as fin:
+    text = fin.read()
+
+patt = r'(?<![A-Z])[A-Z]{3}([a-z])[A-Z]{3}(?![A-Z])'
+results = re.findall(patt, text)
+result = ''.join(results)
+
+print(result)
